64f158d157898f302c4e0b890dd168ba265797e9
Tanishq-Jainop <78198324+Tanishq-Jainop@users.noreply.github.com>
2021-06-14 12:54:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -123,4 +123,33 @@ if(keypress=='72'){
     new_image('spiderman_right_hand.png');
     console.log("h");
 }
+function up(){
+    if(player_y>=0){
+        player_y=player_y-block_image_height;
+        console.log(player_x,player_y)
+        canvas.remove(player_object);
+        player_update()}};
+
+        
+function down(){
+ if(player_y<500){
+     player_y=player_y+block_image_height;
+     console.log(player_x,player_y)
+     canvas.remove(player_object);
+     player_update()}};
+
+     function left(){
+        if(player_x>=0){
+            player_x=player_x-block_image_width;
+            console.log(player_x,player_y)
+            canvas.remove(player_object);
+            player_update()}}
+
+            function right(){
+                if(player_x<=850){
+                    player_x=player_x+block_image_width;
+                    console.log(player_x,player_y)
+                    canvas.remove(player_object);
+                    player_update()}}
+
 }
